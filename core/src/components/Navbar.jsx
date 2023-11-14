92b14d4e0ffc94f1d80de762f851dee2a08435ac
Anukul Pandey <anukul.030601@gmail.com>
2023-11-14 16:01:46 +0530

    feat: display balance

--- core/src/components/Navbar.jsx
+++ core/src/components/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Uik from '@reef-chain/ui-kit';
 import AccountPill from './AccountPill';
+import ReefStateContext from '../context/ReefStateContext';
+import {balanceUtils} from "@reef-chain/util-lib";
+import { BigNumber } from 'ethers';
 
 function Navbar(props) {
+  const { selectedReefSigner } = useContext(ReefStateContext);
 
   return (
     <div className="navbar-container">
       <Uik.ReefLogo className='reef-logo' />
       <div className="accounts-pill">
-        {props.isConnected ? <AccountPill/> :<></>}
+        {props.isConnected ?<div className='navbar-switch'>
+          <AccountPill/>
+          <div className="navbar-balance">
+          <Uik.ReefAmount value={selectedReefSigner?selectedReefSigner.balance+" REEF":'Loading'} />
+          </div>
+        </div>  :<></>}
       </div>
+      
     </div>
   );
 }
