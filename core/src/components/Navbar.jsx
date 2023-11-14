9ca65d15af9ecd8df078812c96e9d497deb6012d
Anukul Pandey <anukul.030601@gmail.com>
2023-11-14 14:35:55 +0530

    feat: account pill

--- core/src/components/Navbar.jsx
+++ core/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Uik from '@reef-chain/ui-kit';
 import AccountPill from './AccountPill';
 
 function Navbar(props) {
+
   return (
     <div className="navbar-container">
       <Uik.ReefLogo className='reef-logo' />
